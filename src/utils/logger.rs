d849625f602014324602ab6e402a349920fea010
zabackary <137591653+zabackary@users.noreply.github.com>
2025-01-07 07:46:51 +0900

    feat: write logs to file

--- src/utils/logger.rs
+++ src/utils/logger.rs
@@ -1,7 +1,33 @@
-use log::{Level, Metadata, Record, SetLoggerError};
-use vexide::{core::println, prelude::Write};
+use alloc::{boxed::Box, sync::Arc};
+use core::time::Duration;
 
-struct SimpleLogger;
+use log::{error, Level, Metadata, Record, SetLoggerError};
+use vexide::{
+    core::{println, sync::Mutex},
+    prelude::{spawn, Write},
+};
+
+struct SimpleLogger {
+    file: Arc<Mutex<vexide::core::fs::File>>,
+}
+// Safety: The brain is single-threaded. Hopefully.
+unsafe impl core::marker::Sync for SimpleLogger {}
+unsafe impl core::marker::Send for SimpleLogger {}
+
+impl SimpleLogger {
+    pub fn new() -> Self {
+        Self {
+            // We're being very unsafe here.
+            #[allow(clippy::arc_with_non_send_sync)]
+            file: Arc::new(Mutex::new(
+                vexide::core::fs::File::options()
+                    .append(true)
+                    .open("log.txt")
+                    .expect("Failed to create log file"),
+            )),
+        }
+    }
+}
 
 impl log::Log for SimpleLogger {
     fn enabled(&self, metadata: &Metadata) -> bool {
@@ -11,6 +37,15 @@ impl log::Log for SimpleLogger {
     fn log(&self, record: &Record) {
         if self.enabled(record.metadata()) {
             println!("{} - {}", record.level(), record.args());
+            if let Some(mut file) = self.file.try_lock() {
+                _ = writeln!(file, "{} - {}", record.level(), record.args());
+            } else {
+                spawn(async {
+                    vexide::async_runtime::time::sleep(Duration::from_millis(100)).await;
+                    error!("Failed to lock log file (slept 100ms). 1 message was ignored.");
+                })
+                .detach();
+            }
         }
     }
 
@@ -19,8 +54,8 @@ impl log::Log for SimpleLogger {
     }
 }
 
-static LOGGER: SimpleLogger = SimpleLogger;
-
 pub fn init() -> Result<(), SetLoggerError> {
-    log::set_logger(&LOGGER).map(|()| log::set_max_level(log::LevelFilter::Debug))
+    let logger = SimpleLogger::new();
+    let logger_ref = Box::leak(Box::new(logger));
+    log::set_logger(logger_ref).map(|()| log::set_max_level(log::LevelFilter::Debug))
 }
