4d1678a79c576b606a9b261c6bdf79872351bd8a
julie0529 <julee28@caj.ac.jp>
2025-01-21 16:29:55 +0900

    Skills part one

--- src/autonomous/skills revised.rs
+++ src/autonomous/skills revised.rs
@@ -14,6 +14,7 @@ impl AutonRoutine<Robot> for Skills {
     type Return = super::Return;
 
     async fn run(&self, robot: &mut Robot) -> Self::Return {
+        //------------------Start to passed middle--------------------//
         // 0 degrees
         robot.drivetrain.reset_inertial(0.0).await?;
         // Place the ring on the stake
@@ -23,26 +24,30 @@ impl AutonRoutine<Robot> for Skills {
         // Drive forward 40cm
         robot.drivetrain.drive_for(420.0).await?;
         // Turn right 90 degrees
-        robot.drivetrain.turn_to(90.0).await?;
+        robot.drivetrain.turn_to(-90.0).await?;
         // Clamp the goal
         robot.clamp.unclamp()?;
-        // Drive backward 40cm
-        robot.drivetrain.drive_for(-370.0).await?;
+        // Drive backward 65cm
+        robot.drivetrain.drive_for(-650.0).await?;
         // Clamp the goal and wait 500ms for the clamp to close
         robot.clamp.clamp()?;
         sleep(Duration::from_millis(500)).await;
-        // Turn to 60 degrees
-        robot.drivetrain.turn_to(54.0).await?;
+        // Turn to 20 degrees
+        robot.drivetrain.turn_to(20.0).await?;
         // Turn the intake on
         robot.intake.run(Direction::Forward).await;
-        // Drive forward 100cm
-        robot.drivetrain.drive_for(1000.0).await?;
-        // Turn to 55 degrees
-        robot.drivetrain.turn_to(53.0).await?;
-        // Drive 1 meter
-        robot.drivetrain.drive_for(1150.0).await?;
-        // Drive backward 30cm
-        robot.drivetrain.drive_for(-250.0).await?;
+        // Drive forward 43cm
+        robot.drivetrain.drive_for(430.0).await?;
+        // Turn to 30 degrees
+        robot.drivetrain.turn_to(30.0).await?;
+        // Drive 80cm
+        robot.drivetrain.drive_for(800.0).await?;
+        sleep(Duration::from_millis(500)).await;
+        // Drive backward 72cm
+        robot.drivetrain.drive_for(72.0).await?;
+
+
+        //---------------------------------------------------------------
         // Turn the intake off
         robot.intake.stop().await;
         // Turn to 180 degrees
