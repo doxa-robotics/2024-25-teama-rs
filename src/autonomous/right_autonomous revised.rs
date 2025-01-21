687777b22ac81389842fb24e28655804ea8a1f36
julie0529 <julee28@caj.ac.jp>
2025-01-21 16:57:10 +0900

    revised auton

--- src/autonomous/right_autonomous revised.rs
+++ src/autonomous/right_autonomous revised.rs
@@ -14,7 +14,7 @@ impl AutonRoutine<Robot> for RightAuton {
     type Return = super::Return;
 
     async fn run(&self, robot: &mut Robot) -> Self::Return {
-        //--------------------grab the stake---------------------------//
+//--------------------grab the stake---------------------------//
         // 0 degrees
         robot.drivetrain.reset_inertial(0.0).await?;
         //turn
@@ -22,9 +22,9 @@ impl AutonRoutine<Robot> for RightAuton {
         //unclamp
         robot.clamp.unclamp()?;
         // Move  64cm
-        robot.drivetrain.drive_for(-64.0).await?;
+        robot.drivetrain.drive_for(-640.0).await?;
         //turn
-        robot.drivetrain.turn_to(220.0).await?;
+        robot.drivetrain.turn_to(140.0).await?;
         // Move  64cm
         robot.drivetrain.drive_for(-200.0).await?;
         //clamp
@@ -33,50 +33,47 @@ impl AutonRoutine<Robot> for RightAuton {
         robot.intake.run(Direction::Forward).await;
         sleep(Duration::from_millis(1000)).await;
         robot.intake.stop().await;
-
-
-        //back 137cm
-        robot.drivetrain.drive_for(1370.0).await?;
         //turn
-        robot.drivetrain.turn_to(90.0).await?;
+        robot.drivetrain.turn_to(-90.0).await?;
+        //turn
+        robot.drivetrain.turn_to(220.0).await?;
         //unclamp
         robot.clamp.unclamp()?;
-        // Turn the intake on
-        robot.intake.run(Direction::Forward).await;
-        //back 50
-        robot.drivetrain.drive_for(500.0).await?;
-        // Turn the intake off
-        robot.intake.stop().await;
-        //turn 180
-        robot.drivetrain.turn_to(180.0).await?;
-        //move forward 10
-        robot.drivetrain.drive_for(100.0).await?;
-        //move back 15
-        robot.drivetrain.drive_for(-150.0).await?;
+        // Move  5cm
+        robot.drivetrain.drive_for(50.0).await?;
         //turn
-        robot.drivetrain.turn_to(25.0).await?;
-        //unclamp
-        robot.clamp.unclamp()?;
-        //move forward 70
-        robot.drivetrain.drive_for(700.0).await?;
+        robot.drivetrain.turn_to(-30.0).await?;
+    
+//--------------------- grab stake 2 and score 2----------------//
+
+        //move
+        robot.drivetrain.drive_for(-250.0).await?;
         //clamp
         robot.clamp.clamp()?;
         //turn
-        robot.drivetrain.turn_to(-48.0).await?;
+        robot.drivetrain.turn_to(90.0).await?;
         // Turn the intake on
         robot.intake.run(Direction::Forward).await;
-        //move forward 60
-        robot.drivetrain.drive_for(600.0).await?;
+        //move 50
+        robot.drivetrain.drive_for(250.0).await?;
+        //turn 180
+        robot.drivetrain.turn_to(180.0).await?;
+        //move forward 50
+        robot.drivetrain.drive_for(500.0).await?;
+        //move back 15
+        robot.drivetrain.drive_for(-150.0).await?;
+
+//------------------------get last ring and get to the ladder-----------------//
         //turn
-        robot.drivetrain.turn_to(-150.0).await?;
-        //move forward 40
-        robot.drivetrain.drive_for(400.0).await?;
+        robot.drivetrain.turn_to(290.0).await?;
+        //move forward 75
+        robot.drivetrain.drive_for(750.0).await?;
+        //turn
+        robot.drivetrain.turn_to(0.0).await?;
         // Turn the intake off
         robot.intake.stop().await;
-        //turn
-        robot.drivetrain.turn_to(-48.0).await?;
-        //move Back to ladder
-        robot.drivetrain.drive_for(900.0).await?;
+        //move forward 60
+        robot.drivetrain.drive_for(450.0).await?;
 
         Ok(())
     }
