90a7b7f74d7c48fe677f606f471820ba1f772e87
zabackary <137591653+zabackary@users.noreply.github.com>
2025-04-21 16:22:06 +0900

    feat: finish adding auton framework

--- src/autons/test.rs
+++ src/autons/test.rs
@@ -1,10 +1,21 @@
-use crate::{subsystems::drivetrain_actions, Robot};
+use crate::{
+    subsystems::drivetrain_actions::{self, CONFIG},
+    Robot,
+};
 
 pub async fn test_auton_red(robot: &mut Robot) {
     robot.tracking.borrow_mut().set_pose((0.0, 0.0, 0.0).into());
     robot
         .drivetrain
-        .action(drivetrain_actions::forward(100.0))
+        .action(drivetrain_actions::forward(1.0, CONFIG))
+        .await;
+    robot
+        .drivetrain
+        .action(drivetrain_actions::forward(1.0, CONFIG))
+        .await;
+    robot
+        .drivetrain
+        .action(drivetrain_actions::turn_to_point((0.0, 0.0).into(), CONFIG))
         .await;
 }
 
--- src/subsystems/drivetrain_actions.rs
+++ src/subsystems/drivetrain_actions.rs
@@ -2,114 +2,78 @@ use core::time::Duration;
 
 use libdoxa::{
     path_planner::cubic_parametric::CubicParametricPath,
-    utils::{pose::Pose, settling::Tolerances},
+    subsystems::drivetrain::actions::config::ActionConfig, utils::pose::Pose,
 };
-use pid::Pid;
-use vexide::prelude::Motor;
 
 const TILES_TO_MM: f64 = 600.0;
 
-pub struct ActionConfig {
-    pub linear_kp: f64,
-    pub linear_ki: f64,
-    pub linear_kd: f64,
+pub const CONFIG: ActionConfig = ActionConfig {
+    linear_kp: 0.0,
+    linear_ki: 0.0,
+    linear_kd: 0.0,
+    turn_kp: 0.0,
+    turn_ki: 0.0,
+    turn_kd: 0.0,
+    pursuit_turn_kp: 0.0,
+    pursuit_turn_ki: 0.0,
+    pursuit_turn_kd: 0.0,
+    pursuit_lookahead: 0.0,
+    linear_error_tolerance: 0.0,
+    linear_velocity_tolerance: 0.0,
+    linear_tolerance_duration: Duration::from_millis(0),
+    linear_timeout: Duration::from_millis(0),
+    turn_error_tolerance: 0.0,
+    turn_velocity_tolerance: 0.0,
+    turn_tolerance_duration: Duration::from_millis(0),
+    turn_timeout: Duration::from_millis(0),
+};
 
-    pub turn_kp: f64,
-    pub turn_ki: f64,
-    pub turn_kd: f64,
-
-    pub pursuit_turn_kp: f64,
-    pub pursuit_turn_ki: f64,
-    pub pursuit_turn_kd: f64,
-}
-
-pub fn forward(distance_tiles: f64) -> impl libdoxa::subsystems::drivetrain::actions::Action {
+pub fn forward(
+    distance_tiles: f64,
+    config: ActionConfig,
+) -> impl libdoxa::subsystems::drivetrain::actions::Action {
     libdoxa::subsystems::drivetrain::actions::ForwardAction::new(
-        {
-            let mut controller = Pid::new(distance_tiles * TILES_TO_MM, Motor::V5_MAX_VOLTAGE);
-            controller.p(0.1, Motor::V5_MAX_VOLTAGE);
-            controller
-        },
-        Tolerances::new()
-            .error_tolerance(5.0)
-            .velocity_tolerance(5.0)
-            .tolerance_duration(Duration::from_millis(200))
-            .timeout(Duration::from_millis(1000)),
+        distance_tiles * TILES_TO_MM,
+        config,
     )
 }
 
-pub fn turn_to_point(point: Pose) -> impl libdoxa::subsystems::drivetrain::actions::Action {
-    libdoxa::subsystems::drivetrain::actions::TurnToPointAction::new(
-        point,
-        {
-            let mut controller = Pid::new(0.0, Motor::V5_MAX_VOLTAGE);
-            controller.p(0.1, Motor::V5_MAX_VOLTAGE);
-            controller
-        },
-        Tolerances::new()
-            .error_tolerance(5.0)
-            .velocity_tolerance(5.0)
-            .tolerance_duration(Duration::from_millis(200))
-            .timeout(Duration::from_millis(1000)),
-    )
+pub fn turn_to_point(
+    point: Pose,
+    config: ActionConfig,
+) -> impl libdoxa::subsystems::drivetrain::actions::Action {
+    libdoxa::subsystems::drivetrain::actions::TurnToPointAction::new(point * TILES_TO_MM, config)
 }
 
-pub fn drive_to_point(point: Pose) -> impl libdoxa::subsystems::drivetrain::actions::Action {
-    libdoxa::subsystems::drivetrain::actions::DriveToPointAction::new(
-        point,
-        {
-            let mut controller = Pid::new(0.0, Motor::V5_MAX_VOLTAGE);
-            controller.p(0.1, Motor::V5_MAX_VOLTAGE);
-            controller
-        },
-        {
-            let mut controller = Pid::new(0.0, Motor::V5_MAX_VOLTAGE);
-            controller.p(0.1, Motor::V5_MAX_VOLTAGE);
-            controller
-        },
-        Tolerances::new()
-            .error_tolerance(5.0)
-            .velocity_tolerance(5.0)
-            .tolerance_duration(Duration::from_millis(200))
-            .timeout(Duration::from_millis(1000)),
-        Tolerances::new()
-            .error_tolerance(5.0)
-            .velocity_tolerance(5.0)
-            .tolerance_duration(Duration::from_millis(200))
-            .timeout(Duration::from_millis(1000)),
-    )
+pub fn drive_to_point(
+    point: Pose,
+    config: ActionConfig,
+) -> impl libdoxa::subsystems::drivetrain::actions::Action {
+    libdoxa::subsystems::drivetrain::actions::DriveToPointAction::new(point * TILES_TO_MM, config)
+}
+
+pub fn boomerang_to_point(
+    point: Pose,
+    config: ActionConfig,
+) -> impl libdoxa::subsystems::drivetrain::actions::Action {
+    libdoxa::subsystems::drivetrain::actions::BoomerangAction::new(point * TILES_TO_MM, config)
 }
 
 pub fn smooth_to_point(
     point: Pose,
     start_easing: f64,
     end_easing: f64,
+    config: ActionConfig,
 ) -> impl libdoxa::subsystems::drivetrain::actions::Action {
     libdoxa::subsystems::drivetrain::actions::LazyAction::new(move |current_pose| {
         libdoxa::subsystems::drivetrain::actions::PurePursuitAction::new(
-            CubicParametricPath::new(current_pose, start_easing, point, end_easing),
-            {
-                let mut controller = Pid::new(0.0, Motor::V5_MAX_VOLTAGE);
-                controller.p(0.1, Motor::V5_MAX_VOLTAGE);
-                controller
-            },
-            {
-                let mut controller = Pid::new(0.0, Motor::V5_MAX_VOLTAGE);
-                controller.p(0.1, Motor::V5_MAX_VOLTAGE);
-                controller
-            },
-            {
-                let mut controller = Pid::new(0.0, Motor::V5_MAX_VOLTAGE);
-                controller.p(0.1, Motor::V5_MAX_VOLTAGE);
-                controller
-            },
-            Tolerances::new()
-                .error_tolerance(5.0)
-                .velocity_tolerance(5.0)
-                .tolerance_duration(Duration::from_millis(200))
-                .timeout(Duration::from_millis(1000)),
-            100.0,
-            Motor::V5_MAX_VOLTAGE / 2.0,
+            CubicParametricPath::new(
+                current_pose,
+                start_easing * TILES_TO_MM,
+                point * TILES_TO_MM,
+                end_easing * TILES_TO_MM,
+            ),
+            config,
         )
     })
 }
